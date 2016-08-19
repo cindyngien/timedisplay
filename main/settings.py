19d9e5bb8a711e494ddc1fc96fbd436fc3c77bfb
Cindy Ngien <cindyngien@gmail.com>
2016-08-18 19:05:25 -0700

    settings changed

--- main/settings.py
+++ main/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'timedisplay', # This name must be lowercase!
+        'USER': 'cindyngien', # you can put whatever you want here
+        'PASSWORD': 'mypassword', # you can put whatever you want here
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -119,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
